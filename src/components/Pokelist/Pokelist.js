380f9ed187d4ef3b7139db224be254f7cd11e435
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-09 15:16:09 -0800

    added and styled images

--- src/components/Pokelist/Pokelist.js
+++ src/components/Pokelist/Pokelist.js
@@ -1,3 +1,4 @@
+import './Pokelist.css';
 export default function Pokelist({ pokemon, currentPage, setCurrentPage, setLoading }) {
   const handleNextPage = () => {
     setCurrentPage((prevState) => ++prevState);
@@ -6,7 +7,10 @@ export default function Pokelist({ pokemon, currentPage, setCurrentPage, setLoad
   return (
     <div>
       {pokemon.map((poke) => (
-        <p key={poke.id}>{poke.pokemon}</p>
+        <>
+          <p key={poke.id}>{poke.pokemon}</p>
+          <img src={poke.url_image}></img>
+        </>
       ))}
       <div>Page: {currentPage}</div>
       <button onClick={handleNextPage}>Next Page</button>

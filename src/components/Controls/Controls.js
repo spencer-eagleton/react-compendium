5f1d85cee52a668cb4a5b9dae930bcbba80264e2
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-09 16:14:29 -0800

    styled list

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,11 +1,19 @@
+import './Controls.css';
 export default function Controls({ query, setQuery, setLoading, order, setOrder }) {
   return (
     <div>
-      <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
-      <select value={order} onChange={(e) => setOrder(e.target.value)}>
-        <option value="asc">Ascending</option>
-        <option value="desc">Descending</option>
-      </select>
+      <div className="controls">
+        <input
+          type="text"
+          placeholder="Name"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <select value={order} onChange={(e) => setOrder(e.target.value)}>
+          <option value="asc">Ascending Order</option>
+          <option value="desc">Descending Order</option>
+        </select>
+      </div>
       <button onClick={() => setLoading(true)}>Search</button>
     </div>
   );
--- src/components/Pokelist/Pokelist.js
+++ src/components/Pokelist/Pokelist.js
@@ -5,15 +5,19 @@ export default function Pokelist({ pokemon, currentPage, setCurrentPage, setLoad
     setLoading(true);
   };
   return (
-    <div>
-      {pokemon.map((poke) => (
-        <>
-          <p key={poke.id}>{poke.pokemon}</p>
-          <img src={poke.url_image}></img>
-        </>
-      ))}
+    <>
+      <div className="poke-list">
+        {pokemon.map((poke) => (
+          <>
+            <div className="poke-card">
+              <p key={poke.id}>{poke.pokemon}</p>
+              <img src={poke.url_image}></img>
+            </div>
+          </>
+        ))}
+      </div>
       <div>Page: {currentPage}</div>
       <button onClick={handleNextPage}>Next Page</button>
-    </div>
+    </>
   );
 }
